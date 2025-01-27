637361d17105aebeec8994eff2d87d26ff969af8
mdrony5134 <rony830388@gmail.com>
2025-01-27 12:47:10 +0600

    issue is working

--- src/components/Dashboard/DashboardLayout.tsx
+++ src/components/Dashboard/DashboardLayout.tsx
@@ -4,20 +4,28 @@ import TopBar from "@/components/Dashboard/components/navigationBar/TopBar";
 import { selectCurrentUser } from "@/redux/features/auth/authSlice";
 import { useAppSelector } from "@/redux/hooks";
 import React, { useEffect, useRef, useState } from "react";
-import { AiOutlineCheckSquare, AiOutlineProduct, AiOutlineTransaction, AiOutlineUser } from "react-icons/ai";
+import {
+  AiOutlineCheckSquare,
+  AiOutlineProduct,
+  AiOutlineTransaction,
+  AiOutlineUser,
+} from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
+import { GiPodiumWinner } from "react-icons/gi";
 import { MdManageHistory, MdOutlineHistory } from "react-icons/md";
 import SideBar from "./components/navigationBar/SiderBar";
-import { GiPodiumWinner } from "react-icons/gi";
+// import { cookies } from "next/headers";
 // import { useGetMeUserQuery } from "@/redux/features/profile/profileApi";
 // import serviceIcon from "@/assets/NavIcons/all-service.svg";
 // import couponIcon from "@/assets/NavIcons/coupon.svg";
 
-
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const user = useAppSelector(selectCurrentUser);
-  const role = user?.role || '';
-  console.log("user",role)
+  // console.log("user", user)
+  // const token = Cookies.get("toekn") ;
+  // console.log("token", token)
+  // const role = user?.role || '';
+  // console.log("user dashboard",role)
   const [isOpen, setIsOpen] = useState(false);
   const navRef = useRef<HTMLDivElement>(null);
   // const {data} = useGetMeUserQuery({})
@@ -41,19 +49,19 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       name: "Dashboard",
       href: "/dashboard",
       icon: AiOutlineProduct,
-      roles:['admin']
+      roles: ["admin"],
     },
     {
       name: "User Dashboard",
       href: "/user-dashboard",
       icon: AiOutlineProduct,
-      roles:['user']
+      roles: ["user"],
     },
     {
       name: "Profile",
       href: "/profile",
       icon: CgProfile,
-      roles:["admin", "user"]
+      roles: ["admin", "user"],
     },
     {
       name: "Transaction History",
@@ -64,41 +72,43 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       name: "Vote History",
       href: "/vote-history",
       icon: MdOutlineHistory,
-      roles:['user']
+      roles: ["user"],
     },
     {
       name: "Product Management",
       href: "/product-management",
       icon: MdManageHistory,
-      roles:['admin']
+      roles: ["admin"],
     },
     {
       name: "User Management",
       href: "/user-management",
       icon: AiOutlineUser,
-      roles:['admin']
+      roles: ["admin"],
     },
     {
       name: "Vote Management",
       href: "/vote-management",
       icon: AiOutlineCheckSquare,
-      roles:['admin']
+      roles: ["admin"],
     },
     {
       name: "Winner History",
       href: "/winner-history",
       icon: GiPodiumWinner,
-      roles:['admin']
+      roles: ["admin"],
     },
   ];
 
-  const filteredMenuItems = navLink.filter(item => item.roles?.includes(role));
+  // const filteredMenuItems = navLink.filter((item) =>
+  //   item.roles?.includes(role)
+  // );
 
   return (
     <div className="flex">
       <div className="max-h-screen h-full sticky top-0 z-50">
         <SideBar
-          navLink={filteredMenuItems}
+          navLink={navLink}
           isOpen={isOpen}
           user={user}
           navRef={navRef}
@@ -106,7 +116,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       </div>
       <div className="w-full">
         <div className="sticky top-0 z-40">
-          <TopBar setIsOpen={setIsOpen} isOpen={isOpen}  />
+          <TopBar setIsOpen={setIsOpen} isOpen={isOpen} />
         </div>
         <div className="bg-[#F6F6F6] min-h-screen"> {children}</div>
       </div>
